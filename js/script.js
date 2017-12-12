25ed5595364e5fd7f56d4a2945efde652a4e04ea
Selivanov <superexpulsion@mail.ru>
2017-12-12 21:29:40 +0300

    удалил лишний код в жс

--- js/script.js
+++ js/script.js
@@ -44,20 +44,3 @@ var storage = localStorage.getItem("nameFeedBack");
 			}
 		}
 	});
-  ymaps.ready(init);
-  var myMap,
-  myPlacemark1;
-
-  function init(){
-    myMap = new ymaps.Map ("map", {
-        center: [34.86591303, -111.76364148],
-        zoom: 15
-    });
-myPlacemark1 = new ymaps.Placemark([34.86591303, -111.763641484], {
-  balloonContentHeader: 'Аризона',
-  balloonContentBody: 'г. Седона',
-  balloonContentFooter: 'Приезжайте',
-  hintContent: 'Здесь самые лучшие места' });
-
-  myMap.geoObjects.add(myPlacemark1);
-  }
